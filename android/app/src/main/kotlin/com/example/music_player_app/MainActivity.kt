fd10b3cd89dc9e847f632d401d0d8946480b74f2
Abhimanyubablet <abhimanyubablet@gmail.com>
2024-02-06 22:23:48 +0530

    update

--- android/app/src/main/kotlin/com/example/music_player_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/music_player_app/MainActivity.kt
@@ -1,6 +1,49 @@
 package com.example.music_player_app
 
+import android.content.ContentValues
+import android.content.Context
+import android.media.RingtoneManager
+import android.net.Uri
+import android.provider.MediaStore
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
+
+    private val CHANNEL = "abhi"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+            if (call.method == "setRingtone") {
+                val filePath = call.argument<String>("filePath")
+                if (filePath != null) {
+                    setRingtone(filePath)
+                    result.success(null)
+                } else {
+                    result.error("INVALID_ARGUMENT", "File path is null", null)
+                }
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+
+    private fun setRingtone(filePath: String) {
+        val context: Context = applicationContext
+        val values = ContentValues().apply {
+            put(MediaStore.MediaColumns.DATA, filePath)
+            put(MediaStore.MediaColumns.TITLE, "Custom Ringtone")
+            put(MediaStore.MediaColumns.MIME_TYPE, "audio/x-wav")
+            put(MediaStore.Audio.Media.IS_RINGTONE, true)
+        }
+
+        val uri: Uri? = context.contentResolver.insert(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, values)
+        uri?.let {
+            RingtoneManager.setActualDefaultRingtoneUri(context, RingtoneManager.TYPE_RINGTONE, it)
+        }
+    }
+
 }
--- lib/views/player.dart
+++ lib/views/player.dart
@@ -1,60 +1,123 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:flutter_ringtone_player/flutter_ringtone_player.dart';
+import 'package:on_audio_query/on_audio_query.dart';
 import 'package:music_player_app/consts/colors.dart';
 import 'package:music_player_app/consts/text_style.dart';
 import 'package:music_player_app/controllers/player_controller.dart';
-import 'package:on_audio_query/on_audio_query.dart';
 
 class Player extends StatelessWidget {
   final List<SongModel> data;
-  const Player({super.key, required this.data});
+
+  const Player({Key? key, required this.data}) : super(key: key);
+
+  Future setRingtone(String filePath, BuildContext context) async {
+    const platform = MethodChannel('abhi');
+    try {
+      await platform.invokeMethod('setRingtone', {'filePath': filePath,'mimeType': 'audio/x-wav'});
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Ringtone set successfully!')),
+      );
+    } on PlatformException catch (e) {
+      print("Failed to set ringtone: '${e.message}'.");
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Failed to SetRingtone')),
+      );
+    }
+  }
+
 
   @override
   Widget build(BuildContext context) {
     var controller = Get.find<PlayerController>();
+
+    void showRingtoneConfirmationDialog(String filePath) {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Set as Ringtone'),
+            content: SingleChildScrollView(
+              child: ListBody(
+                children: <Widget>[
+                  Text('Do you want to set this song as your ringtone?'),
+                ],
+              ),
+            ),
+            actions: <Widget>[
+              TextButton(
+                child: Text('Cancel'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+              TextButton(
+                child: Text('Set as Ringtone'),
+                onPressed: () {
+                  setRingtone(filePath,context);
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
+
     return Scaffold(
       backgroundColor: bgColor,
-      appBar: AppBar(),
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            icon: Icon(Icons.more_vert),
+            onPressed: () {
+              String? filePath = data[controller.playIndex.value].uri;
+              showRingtoneConfirmationDialog(filePath!);
+            },
+          ),
+        ],
+      ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(
           children: [
-            Obx( () => Expanded(
-                  child: Container(
-                    clipBehavior: Clip.antiAliasWithSaveLayer,
+            Obx(() => Expanded(
+              child: Container(
+                clipBehavior: Clip.antiAliasWithSaveLayer,
                 height: 300,
                 width: 300,
-                decoration: const BoxDecoration(
+                decoration: BoxDecoration(
                   shape: BoxShape.circle,
                   color: Colors.red,
                 ),
                 alignment: Alignment.center,
-                // child: const Icon(Icons.music_note,size: 40,color: whiteColor,),
                 child: QueryArtworkWidget(
-                    id: data[controller.playIndex.value].id,
-                    type: ArtworkType.AUDIO,
-                    artworkHeight: double.infinity,
-                    artworkWidth: double.infinity,
-                    nullArtworkWidget: Icon(Icons.music_note,size: 48,color: whiteColor,),
+                  id: data[controller.playIndex.value].id,
+                  type: ArtworkType.AUDIO,
+                  artworkHeight: double.infinity,
+                  artworkWidth: double.infinity,
+                  nullArtworkWidget: Icon(
+                    Icons.music_note,
+                    size: 48,
+                    color: whiteColor,
+                  ),
                 ),
-
-              )),
-            ),
-            SizedBox(
-              height: 13,
-            ),
+              ),
+            )),
+            SizedBox(height: 13),
             Expanded(
               child: Container(
                 padding: EdgeInsets.all(8),
                 alignment: Alignment.center,
-                decoration: const BoxDecoration(
+                decoration: BoxDecoration(
                   color: whiteColor,
                   borderRadius: BorderRadius.vertical(
                     top: Radius.circular(16),
                   ),
                 ),
                 child: Obx(
-                  ()=> Column(
+                      () => Column(
                     children: [
                       Text(
                         data[controller.playIndex.value].displayNameWOExt,
@@ -67,9 +130,7 @@ class Player extends StatelessWidget {
                           size: 14,
                         ),
                       ),
-                      SizedBox(
-                        height: 13,
-                      ),
+                      SizedBox(height: 13),
                       Text(
                         data[controller.playIndex.value].artist.toString(),
                         textAlign: TextAlign.center,
@@ -81,44 +142,42 @@ class Player extends StatelessWidget {
                           size: 20,
                         ),
                       ),
-                      SizedBox(
-                        height: 13,
-                      ),
+                      SizedBox(height: 13),
                       Obx(
-                        () => Row(
+                            () => Row(
                           children: [
                             Text(
                               controller.position.value,
                               style: ourStyle(color: bgDarkColor),
                             ),
                             Expanded(
-                                child: Slider(
-                                    thumbColor: slideColor,
-                                    inactiveColor: bgColor,
-                                    activeColor: slideColor,
-                                    min: Duration(seconds:0).inSeconds.toDouble(),
-                                    max: controller.max.value,
-                                    value: controller.value.value,
-                                    onChanged: (newValue) {
-                                      controller.changeDurationToSeconds(newValue.toInt());
-                                      newValue= newValue;
-                                    })),
+                              child: Slider(
+                                thumbColor: slideColor,
+                                inactiveColor: bgColor,
+                                activeColor: slideColor,
+                                min: Duration(seconds: 0).inSeconds.toDouble(),
+                                max: controller.max.value,
+                                value: controller.value.value,
+                                onChanged: (newValue) {
+                                  controller.changeDurationToSeconds(newValue.toInt());
+                                  newValue = newValue;
+                                },
+                              ),
+                            ),
                             Text(
-                                controller.duration.value,
+                              controller.duration.value,
                               style: ourStyle(color: bgDarkColor),
                             ),
                           ],
                         ),
                       ),
-                      SizedBox(
-                        height: 13,
-                      ),
+                      SizedBox(height: 13),
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceAround,
                         children: [
                           IconButton(
                             onPressed: () {
-                              controller.playSong(data[controller.playIndex.value-1].uri,controller.playIndex.value-1);
+                              controller.playSong(data[controller.playIndex.value - 1].uri, controller.playIndex.value - 1);
                             },
                             icon: Icon(
                               Icons.skip_previous_rounded,
@@ -127,37 +186,40 @@ class Player extends StatelessWidget {
                             ),
                           ),
                           Obx(
-                            () => CircleAvatar(
+                                () => CircleAvatar(
                               radius: 35,
                               backgroundColor: bgColor,
                               child: Transform.scale(
                                 scale: 2.5,
                                 child: IconButton(
-                                    onPressed: () {
-                                      if (controller.isPlaying.value){
-                                         controller.audioPlayer.pause();
-                                         controller.isPlaying(false);
-                                      }else{
-                                        controller.audioPlayer.play();
-                                        controller.isPlaying(true);
-                                      }
-                                    },
-                                    icon: controller.isPlaying.value ?Icon(
-                                      Icons.pause,
-                                      color: whiteColor,
-                                    ): Icon(
-                                      Icons.play_arrow_rounded,
-                                      color: whiteColor,
-                                    )),
+                                  onPressed: () {
+                                    if (controller.isPlaying.value) {
+                                      controller.audioPlayer.pause();
+                                      controller.isPlaying(false);
+                                    } else {
+                                      controller.audioPlayer.play();
+                                      controller.isPlaying(true);
+                                    }
+                                  },
+                                  icon: controller.isPlaying.value
+                                      ? Icon(
+                                    Icons.pause,
+                                    color: whiteColor,
+                                  )
+                                      : Icon(
+                                    Icons.play_arrow_rounded,
+                                    color: whiteColor,
+                                  ),
+                                ),
                               ),
                             ),
                           ),
                           IconButton(
-                              onPressed: () {
-                                controller.playSong(data[controller.playIndex.value+1].uri,controller.playIndex.value+1);
-                              },
-                              icon: Icon(Icons.skip_next_rounded,
-                                  size: 40, color: bgDarkColor)),
+                            onPressed: () {
+                              controller.playSong(data[controller.playIndex.value + 1].uri, controller.playIndex.value + 1);
+                            },
+                            icon: Icon(Icons.skip_next_rounded, size: 40, color: bgDarkColor),
+                          ),
                         ],
                       )
                     ],
